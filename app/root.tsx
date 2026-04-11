eca8fc55124d313c0cf4c921d18949d07824defa
Oscar Moreno <os.kardavid70@gmail.com>
2026-04-11 15:21:00 -0600

    chore: update shopify cdn

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <meta name="shopify-api-key" content="7de20cb4465f53fb2a2b724d6cbed624" />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <link
           rel="stylesheet"
           href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
